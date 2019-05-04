6c299ce2936d923f04a07ee0cb0bde9f196ae085
unknown <kelli.okuji@gmail.com>
2019-05-04 13:09:20 -0700

    Update with revised htmls

---
+++ schema.sql
@@ -0,0 +1,8 @@
+create eeoc
+-- Connect to the 'master' database to run this snippet
+USE eeoc
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROeeocGO
